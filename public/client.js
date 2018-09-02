f259d952035ae1472af42a65c0a8d675d6291497
Dylan Makowski <dmakowski@hacknet.local>
2018-09-02 09:03:21 -0400

    Added database feature w/ counter

--- public/client.js
+++ public/client.js
@@ -8,4 +8,31 @@ console.log("Client side code running....");
 var button = document.getElementById('penryButton');
 button.addEventListener('click', function(e) {
   console.log('Button was clicked!');
+
+  fetch('/clicked', {method: 'POST'})
+  .then(function(response) {
+    if(response.ok) {
+      console.log('Click was recorded');
+      return;
+    }
+    throw new Error('Request Failed...');
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
 });
+
+//This just queries the database every second to get data 
+setInterval(function() {
+  fetch('/clicked', {method: 'GET'})
+  .then(function(response) {
+    if(response.ok) return response.json();
+    throw new Error('Request Failed :(');
+  })
+  .then(function(data) {
+    document.getElementById('counter').innerHTML = `Penry was booped ${data.length} times`;
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+}, 1000);
--- website.js
+++ website.js
@@ -2,21 +2,60 @@ var express = require('express');
 var app = express();
 //router just allows better routing within express
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
 //Helps serve static files
 app.use(express.static(__dirname + '/public'));
+
+// connect to the database
+let db;
+
+
+var url = 'mongodb://dmakowski:Kratos%23716@localhost:27017/admin';
+
+//connects to the database using the URL, auth, and database name
+MongoClient.connect(url, function (err, database) {
+  if (err) {
+    return console.log(err);
+  }
+  db = database;
+
+  //Starts the server
+  app.listen(4444, function() {
+    console.log("Server is running on port 4444");
+  });
+});
 
 //declares variable to call public dir
 var path = __dirname + '/public/';
 
 app.use('/', router);
 
+//serves home page
 router.get('/', function(req, res) {
   res.sendFile(path + 'index.html');
 });
 
+//Create post request for /clicked which will create a document in the database
+app.post('/clicked', (req, res) => {
+  var click = {clickTime: new Date()};
+  console.log(click);
+  console.log(db);
 
-//Starts the server
-app.listen(4444, function() {
-  console.log("Server is running on port 4444");
+  db.collection('clicks').save(click, function (err, result)  {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Click added to DB!');
+    res.sendStatus(201);
+  });
+});
+
+
+// get the click data from the database
+app.get('/clicked', (req, res) => {
+  db.collection('clicks').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    res.send(result);
+  });
 });
